b2206b83e245a265f8e8c0f5351d813a5c2e740e
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-04-27 21:22:38 +0530

    OpenWeatherMap API Tests

---
+++ src/Api4.jsx
@@ -0,0 +1,65 @@
+import React, {useState, useEffect} from 'react';
+import "./Api4.css";
+import axios from "axios";
+
+
+function Api4() {
+    // const [cityname, setcityname] = useState("")
+    const API_KEY = "83ae873f99e8cca64fd2e00d659b24be"
+    const [Id, setId] = useState();
+    const [Maahol, setMaahol] = useState();
+    const [Desc, setDesc] = useState("");
+    const [Humi, setHumi] = useState();
+    const [Tempr, setTempr] = useState()
+
+    const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=Bern&appid=${API_KEY}`;
+
+    useEffect(() => {
+        async function getData(){
+            const result = await axios.get(apiCall)
+             setId(result.data.name);
+             setMaahol(result.data.weather[0].main);
+             setDesc(result.data.weather[0].description);
+             setHumi(result.data.main.humidity);
+             setTempr(result.data.main.temp);
+        }
+
+        console.log(getData);
+        // return () => {
+        //     cleanup
+        // }
+        getData();
+    })
+
+    return (
+        <div className="weather__app">
+            {/* <input placeholder="City Name"/> 
+            <button>Find Weather</button> */}
+            
+            <div className="temp__cont">
+                <div className="temp__contl">
+                    {Tempr}
+                </div>
+
+                <div className="temp__contr">
+                    {Humi}
+                </div>
+            </div>
+
+            <div>
+                <h1>City ID is: {Id}</h1>
+            </div>
+
+            <div>
+                <div>
+                    <h3>It is currently {Maahol}</h3>
+                </div>
+                <p>
+                    {Desc}
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Api4
--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 // import Api1 from './Api1';
 // import Api2 from './Api2';
-import Api3 from './Api3';
+// import Api3 from './Api3';
+import Api4 from './Api4';
 
 function App() {
   return (
     <div className="App">
       {/* <Api1 /> */}
       {/* <Api2 /> */}
-      <Api3 />
+      {/* <Api3 /> */}
+      <Api4 />
     </div>
   );
 }
