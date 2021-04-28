faa6f96557ef9fea990d35deead5212a806bb80f
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-04-28 22:40:35 +0530

    playing with cards

--- src/MaterialUI.jsx
+++ src/MaterialUI.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './MaterialUI.css'
 import { Card, CardContent } from '@material-ui/core';
 
 function MaterialUI() {
     return (
         <div>
-            <Card className="material__card">
-                <CardContent>
+            <Card variant="outlined" className="material__card">
+                <CardContent className="card__content">
                     <h2>Material Ui Card</h2>
                     <h4>Card Less Valuable Info</h4>
                     <p>Least Valuable</p>
