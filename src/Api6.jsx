90f11e10f8cf271abcb81abc89c4b23796d41ac6
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-04-28 21:44:47 +0530

    Added Multiple states c19 v6

--- src/Api6.jsx
+++ src/Api6.jsx
@@ -12,6 +12,18 @@ function Api6() {
     const [sDea, setsDea] = useState();
     const [sRec, setsRec] = useState();
 
+    const [sName2, setSName2] = useState();
+    const [sActive2, setsActive2] = useState();
+    const [sConf2, setsConf2] = useState();
+    const [sDea2, setsDea2] = useState();
+    const [sRec2, setsRec2] = useState();
+
+    const [sName3, setSName3] = useState();
+    const [sActive3, setsActive3] = useState();
+    const [sConf3, setsConf3] = useState();
+    const [sDea3, setsDea3] = useState();
+    const [sRec3, setsRec3] = useState();
+
     useEffect(() => {
         async function GetState(){
             const ret = await axios.get(c19);
@@ -20,6 +32,18 @@ function Api6() {
             setsActive(ret.data.statewise[1].active);
             setsRec(ret.data.statewise[1].recovered)
             setsDea(ret.data.statewise[1].deaths)
+
+            setSName2(ret.data.statewise[16].state);
+            setsConf2(ret.data.statewise[16].confirmed)
+            setsActive2(ret.data.statewise[16].active);
+            setsRec2(ret.data.statewise[16].recovered)
+            setsDea2(ret.data.statewise[16].deaths)
+
+            setSName3(ret.data.statewise[10].state);
+            setsConf3(ret.data.statewise[10].confirmed)
+            setsActive3(ret.data.statewise[10].active);
+            setsRec3(ret.data.statewise[10].recovered)
+            setsDea3(ret.data.statewise[10].deaths)
         }
         // return () => {
         //     cleanup
@@ -45,18 +69,18 @@ function Api6() {
                     <td>{sDea}</td>
                 </tr>
                 <tr>
-                    <td>State</td>
-                    <td>Deaths</td>
-                    <td>Confirmed</td>
-                    <td>Deaths</td>
-                    <td>Confirmed Today</td>
+                    <td>{sName2}</td>
+                    <td>{sActive2}</td>
+                    <td>{sConf2}</td>
+                    <td>{sRec2}</td>
+                    <td>{sDea2}</td>
                 </tr>
                 <tr>
-                    <td>State</td>
-                    <td>Deaths</td>
-                    <td>Confirmed</td>
-                    <td>Deaths</td>
-                    <td>Confirmed Today</td>
+                    <td>{sName3}</td>
+                    <td>{sActive3}</td>
+                    <td>{sConf3}</td>
+                    <td>{sRec3}</td>
+                    <td>{sDea3}</td>
                 </tr>
             </table>
         </div>
