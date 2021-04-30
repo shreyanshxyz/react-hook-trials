a754962fc02452cad96295eb7313df166f3f6e90
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-04-30 19:18:32 +0530

    card + dropdown

--- src/Mui.jsx
+++ src/Mui.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./Mui.css";
-import { Select, MenuItem } from "@material-ui/core";
+import { Card, CardContent, Select, MenuItem } from "@material-ui/core";
 
 function Mui() {
   return (
     <div className="drp__test">
       <div className="form__container">
-        <Select defaultValue="0" autoWidth={true}>
+        <Select
+          className="drop__style"
+          defaultValue="0"
+          variant="outlined"
+          autoWidth={true}
+        >
           <MenuItem value={1}>Madhya Pradesh</MenuItem>
           <MenuItem value={2}>Uttar Pradesh</MenuItem>
           <MenuItem value={3}>Rajasthan</MenuItem>
@@ -16,11 +21,61 @@ function Mui() {
           <MenuItem value={7}>Kerala</MenuItem>
           <MenuItem value={8}>Karnataka</MenuItem>
           <MenuItem value={9}>Assam</MenuItem>
-          <MenuItem value={0}>Select a state</MenuItem>
+          <MenuItem value={0}>India</MenuItem>
         </Select>
       </div>
 
-      <div>Card</div>
+      <div className="card__container1">
+        <Card className="material__card">
+          <CardContent>
+            <h2>Confirmed</h2>
+            <h4>+1000</h4>
+            <h3>31000</h3>
+          </CardContent>
+        </Card>
+
+        <Card className="material__card">
+          <CardContent>
+            <h2>Active Cases</h2>
+            <h4>+1000</h4>
+            <h3>31000</h3>
+          </CardContent>
+        </Card>
+
+        <Card className="material__card">
+          <CardContent>
+            <h2>Recovered</h2>
+            <h4>+1000</h4>
+            <h3>31000</h3>
+          </CardContent>
+        </Card>
+      </div>
+
+      <div className="card__container2">
+        <Card className="material__card">
+          <CardContent>
+            <h2>Deaths</h2>
+            <h4>+1000</h4>
+            <h3>31000</h3>
+          </CardContent>
+        </Card>
+
+        <Card className="material__card">
+          <CardContent>
+            <h2>Tested</h2>
+            <h4>+1000</h4>
+            <h3>31000</h3>
+          </CardContent>
+        </Card>
+
+        <Card className="material__card">
+          <CardContent>
+            <h2>Active Cases</h2>
+            <h4>+1000</h4>
+            <h3>31000</h3>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
