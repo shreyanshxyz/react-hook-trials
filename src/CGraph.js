6c11132719615e7067b7ae888efc2425c86a973a
shreyanshxyz <shreyanshbhadoria@gmail.com>
2021-05-06 22:32:55 +0530

    Added realtime api data to make a dynamic graph

--- src/CGraph.js
+++ src/CGraph.js
@@ -7,25 +7,37 @@ function CGraph() {
   const [chartData, setchartData] = useState();
 
   const chart = () => {
+    let confCase = [];
+    let upDate = [];
+
     axios
       .get("https://api.covid19india.org/data.json")
       .then((res) => {
-        console.log(res.data.cases_time_series);
+        // console.log(res.data.cases_time_series);
+        const lastDate = res.data.cases_time_series.length - 1;
+        var i;
+        for (i = 0; i < lastDate; i++) {
+          confCase.push(parseInt(res.data.cases_time_series[i].dailyconfirmed));
+          upDate.push(res.data.cases_time_series[i].dateymd);
+        }
+        setchartData({
+          labels: upDate,
+          datasets: [
+            {
+              label: "Covid Cases",
+              data: confCase,
+              backgroundColor: "purple",
+              borderWidth: 4,
+              fill: true,
+            },
+          ],
+        });
       })
       .catch((err) => {
         console.log(err);
       });
-    setchartData({
-      labels: ["monday", "tuesday", "wednesday", "thursday", "friday"],
-      datasets: [
-        {
-          label: "Covid Cases",
-          data: [31, 55, 1, 23, 87],
-          backgroundColor: "rgba(255, 99, 132, 0.2)",
-          borderWidth: 4,
-        },
-      ],
-    });
+
+    console.log(confCase, upDate);
   };
 
   useEffect(() => {
@@ -36,27 +48,7 @@ function CGraph() {
       <h1>Graph</h1>
       <p>Using Chart.js (preferably)</p>
       <div className="graph__static">
-        <Line
-          data={chartData}
-          options={{
-            responsive: true,
-            title: { text: "Graph Static", display: true },
-            scales: {
-              yAxes: [
-                {
-                  ticks: {
-                    autoSkip: true,
-                    maxtickslimit: 10,
-                    beginatzero: true,
-                  },
-                  gridLines: {
-                    display: false,
-                  },
-                },
-              ],
-            },
-          }}
-        />
+        <Line data={chartData} />
       </div>
     </div>
   );
